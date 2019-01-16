8e4d86a6814200d3603c2f1f8b53495fbc61b0fe
Erik Adrian Durán <daftpunkguy11@gmail.com>
2019-01-15 18:11:00 -0600

    Secciones de landing Berkeley

---
+++ Thanks.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>¡Gracias por tu interés!</title>
+    <link rel="stylesheet" href="assets/scss/stylesheet.css">
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/png" href="assets/img/png/favicon.png" />
+    <link rel="shortcut icon" type="image/png" href="assets/img/png/favicon.png" />
+
+</head>
+
+<body>
+    <div class="gracias-container">
+        <div class="dialog-container">
+            <div class="logos-container">
+                <img src="assets/img/svg/IESE-Business-School.svg" alt="IESE">
+                <img src="assets/img/png/logo-execed-150x81.png" alt="Executive Education">
+            </div>
+            <div class="dialog-cont">
+                <div class="grapBo fs30 negro19">
+                    ¡Gracias!
+                </div>
+                <div class="grapRe fs16 negro19">
+                    Gracias por tu interés en el programa
+                </div>
+                <div class="grapBo fs16 gris70">
+                    "Mindset Digital: Innovación y liderazgo en la era digital”
+                </div>
+                <div class="grapRe fs16 negro46">
+                    Si tu descarga no comienza <a class=" grapBo rojo39" href="">da click aquí</a>
+                </div>
+            </div>
+            <div class="gracias-linea"></div>
+            <div class="gracias-button">
+                <div class="grapBo fs20 negro19">
+                    ¿Listo para el próximo paso?
+                </div>
+                <a class="btn-gracias grapBo fs16 blanco" href="#" target="_blank" rel="noopener noreferrer">
+                    INSCRIBIRTE
+                    <img src="assets/img/svg/long-arrow-alt-right-solid.svg" alt="arrow">
+                </a>
+            </div>
+            <div class="gracias-redes">
+                <div class="grapRe fs14 negro3D">
+                    Conéctate con nosotros
+                </div>
+                <div class="icon-redes">
+                    <a href="#" target="_blank" rel="noopener noreferrer">
+                        <img src="assets/img/svg/facebook-f-brands.svg" alt="Facebook">
+                    </a>
+                    <a href="#" target="_blank" rel="noopener noreferrer">
+                        <img src="assets/img/svg/twitter-brands.svg" alt="Twiter">
+                    </a>
+                    <a href="#" target="_blank" rel="noopener noreferrer">
+                        <img src="assets/img/svg/linkedin-in-brands.svg" alt="Linkedin">
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
