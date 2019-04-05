9cf317d1f23511e1d78aaa36710e1d5f3fcd66b3
Erik Adrian Durán <daftpunkguy11@gmail.com>
2019-04-04 18:18:31 -0600

    pagina terminos del servicio

---
+++ terminos-del-servicio.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="utf-8">
+    <title>Términos del Servicio</title>
+    <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="assets/scss/stylesheet.css">
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/png" href="assets/img/favicon.ico" />
+</head>
+<body>
+    
+</body>
+</html>
+<div class="terminos-condiciones-cookies-condiciones">
+    <div class="terminos-title ralBo fs32 negro3D">
+        Términos del Servicio
+    </div>
+    <div class="terminos-container opsRe fs14 negro3D">
+        Este sitio web está operado y es propiedad de Eruditus Learning Solutions Pte. Ltd. 
+        (Singapore Company Registration Number: 201230726W), de ahora en adelante conocido 
+        como <strong>"ERUDITUS"</strong>. Por favor, lee los términos de servicio ("TDS") y la <a class="rojo39" href="" target="_blank" rel="noopener noreferrer"> 
+        política de privacidad</a> de ERUDITUS antes de utilizar cualquier parte del sitio web de ERUDITUS 
+        (el “sitio”, mismo que consiste de todo el contenido y páginas ubicadas en el dominio 
+        Eruditus.com), incluyendo al acceso a cualquier material de un curso, chat rooms y 
+        otros servicios electrónicos. Estos TDS y la política de privacidad son acuerdos 
+        (los “acuerdos” entre tú y ERUDITUS). Al utilizar el sitio aceptas los acuerdos, 
+        independientemente de si eres un usuario registrado o no. Si no entiendes o no 
+        deseas estar sujeto a los términos de los acuerdos, no deberías utilizar el sitio.
+        <br>
+        <br>
+        ERUDITUS se reserva el derecho a modificar los TDS en cualquier momento sin previo aviso. 
+        Cualquier cambio en estos TDS serán efectivos de manera inmediata una vez que se publiquen 
+        en la página con una fecha de actualización. Al acceder al sitio después de que se hayan 
+        hecho los cambios aceptas los acuerdos en carácter prospectivo a los TDS modificados y a 
+        todos los cambios. Asegúrate de regresar a esta página periódicamente para estar familiarizado 
+        con la versión más reciente de estos TDS.
+        <br>
+        <br>
+        Aceptas que eres responsable del uso que le des al sitio y de tus publicaciones como usuario. 
+        “Publicaciones como usuario” incluye todo el contenido subido, publicado o distribuido en el 
+        sitio por ti o por otros usuarios del sitio, incluyendo, pero no limitándose, a todas las 
+        publicaciones en los foros, wiki edits, notas, preguntas, comentarios, videos y archivos. 
+        Aceptas utilizar el sitio aceptando todos estos TDS y según todas las leyes aplicables locales, 
+        estatales, nacionales e internacionales, reglas y regulaciones, incluyendo leyes de derechos 
+        de autor, cualquier ley relacionada con la transmisión de datos técnicos exportada de tu país 
+        de residencia, y todas las leyes de Singapur.
+        <br>
+        <br>
+        <ol>
+            <li>
+                <div class="fs14 opsBo negro3D">REGLAS DE CONDUCTA EN LÍNEA</div>
+                <div class="fs14 opsRe negro3D">
+                    Como condición para poder utilizar los servicios de ERUTIDUS, no utilizarás el sitio 
+                    de ninguna manera que tenga como objetivo dañar, inutilizar, sobrecargar o dañar 
+                    cualquier servidor de ERUDITUS o de la red(es) conectadas a cualquier servidor de 
+                    ERUDITUS o interferir con cualquier otro uso que le dé un tercero y el disfrute del 
+                    sitio. No intentarás obtener acceso sin autorización al sitio, otras cuentas, sistemas 
+                    computaciones o redes conectadas a cualquier servidor de ERUDITUS a través de hacking, 
+                    extracción de contraseñas o cualquier otro medio. No podrás obtener o intentar obtener 
+                    cualquier material o información almacenado en el sitio, los servidores o computadoras 
+                    asociadas a través de cualquier medio, si no están intencionalmente disponibles en el 
+                    sitio.
+                </div> 
+            </li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ol>
+    </div>
+</div>
